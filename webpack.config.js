1b4a8d0793cdcd241cf9aee0597f8fd18d00ec7b
ayatoy <ayatoy@gmail.com>
2021-06-22 13:53:14 +0900

    秘密鍵が存在しない場合に自動で秘密鍵を生成する機能を追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const KintonePlugin = require('@kintone/webpack-plugin-kintone-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
@@ -24,13 +23,6 @@ module.exports = {
     path: path.resolve(__dirname, 'plugin'),
     filename: '[name].bundle.js',
   },
-  plugins: [
-    new KintonePlugin({
-      manifestJSONPath: './plugin/manifest.json',
-      privateKeyPath: './private.ppk',
-      pluginZipPath: './dist/plugin.zip',
-    }),
-  ],
   target: ['web', 'es5'],
   optimization: {
     minimize: true,
